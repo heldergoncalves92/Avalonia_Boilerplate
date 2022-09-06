8f3141a262431b3f42023a2bacb7ecc2c1bd4824
Hélder Gonçalves <helder.goncalves@outsystems.com>
2022-09-06 16:03:10 +0100

    Reproduce issue

--- Avalonia.Boilerplate/MainWindow.xaml.cs
+++ Avalonia.Boilerplate/MainWindow.xaml.cs
@@ -1,23 +1,41 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Chrome;
+using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 using System.Threading.Tasks;
 
 namespace Avalonia.Boilerplate {
     public class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            Position = new PixelPoint((int)-8,(int)-8);
+            Width = Screens.Primary.WorkingArea.Width+ 16;
+            Height = Screens.Primary.WorkingArea.Height + 16;
             WindowState = WindowState.Maximized;
-            
+
 #if DEBUG
             this.AttachDevTools();
 #endif
         }
 
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {
+            base.OnApplyTemplate(e);
+
+            var titleBar = this.FindDescendantOfType<TitleBar>();
+            if (titleBar != null)
+            {
+                titleBar.IsVisible = false;
+            }
+
+        }
+
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
-            var button = this.FindControl<Button>("MyButton");
-            button.Click += Button_Click;  
+            //var button = this.FindControl<Button>("MyButton");
+            //button.Click += Button_Click;  
         }
 
         private async void Button_Click(object? sender, Interactivity.RoutedEventArgs e)
@@ -26,8 +44,8 @@ namespace Avalonia.Boilerplate {
             //await Task.Delay(3000);
             var appWindow = new MyWindow();
             appWindow.WindowState = WindowState.Maximized;
-            appWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            appWindow.Show(this);
+            //appWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            appWindow.Show();
         }
     }
 }
