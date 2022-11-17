d692ae080efd339278722d94305e30c4b6f0aad7
heldergoncalves92 <heldergoncalves92@gmail.com>
2022-11-17 10:17:21 +0000

    Window is no maximized

--- Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
@@ -1,18 +1,16 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 
 namespace Avalonia.Boilerplate {
     public class MainWindow : Window {
         public MainWindow() {
-            InitializeComponent();
-#if DEBUG
-            this.AttachDevTools();
-#endif
-        }
-
-        private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
+
+            // By setting the Height property we lost the Maximized state. Is it expected?
+            Height = 718;
+            WindowState = WindowState.Maximized;
         }
     }
 }
---
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/MyWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia.Boilerplate
+{
+    internal class MyWindow : Window {
+
+        public MyWindow()
+        {
+            AvaloniaXamlLoader.Load(this);
+            WindowState = WindowState.Maximized;
+            CanResize = true;
+            ExtendClientAreaToDecorationsHint = true;
+            ExtendClientAreaTitleBarHeightHint = -1;
+            Height = 718;
+            //Width = 1424;
+
+        }
+
+        protected override void HandleWindowStateChanged(WindowState state)
+        {
+            base.HandleWindowStateChanged(state);
+            Console.Out.WriteLine($"State Changed: {state}");
+        }
+    }
+}
